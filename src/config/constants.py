81a7ba003941cf8e6cf28f8fa0eba3f147a19bce
Tim Bryant <Timothybryant3@gmail.com>
2023-08-21 12:50:57 -0400

    refactor: Update package name and config file path

--- src/config/constants.py
+++ src/config/constants.py
@@ -3,5 +3,5 @@ import os
 GET_WAN_IP = "https://api.ipify.org/"
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 CONFIG_PATH_DEFAULT = os.path.join(
-    os.getenv("HOME", ""), ".config", "pez-sm", "config.yaml"
+    os.getenv("HOME", ""), ".config", "psi", "config.yaml"
 )
